e8900736d6bf6c9a59acd4fcbc36f99d41b9654c
moshea <oshea00@gmail.com>
2020-08-16 18:22:00 +0000

    scope checking

--- backend/auth.py
+++ backend/auth.py
@@ -1,5 +1,11 @@
-from get_token import get_token
-from verify_token import verify_token
+import os
+import json
+
+from six.moves.urllib.request import urlopen
+from jose import jwt
+
+AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN")
+AUTH0_API_ID = os.environ.get("AUTH0_API_ID")
 
 def handler(event, context):
     print(event)
@@ -41,3 +47,60 @@ def generate_policy(principal_id, effect, resource, scopes=None):
     if scopes:
         policy['context'] = {'scopes': scopes}
     return policy
+
+def get_token(event):
+    # whole_auth_token should look like:
+    # "Bearer SOME_CODE_GIBBERISH6r712fyasd.othergibberish.finalgibberish"
+    whole_auth_token = event.get('authorizationToken')
+    print('Client token: ' + whole_auth_token)
+    print('Method ARN: ' + event['methodArn'])
+    if not whole_auth_token:
+        raise Exception('Unauthorized')
+    token_parts = whole_auth_token.split(' ')
+    auth_token = token_parts[1]
+    token_method = token_parts[0]
+    if not (token_method.lower() == 'bearer' and auth_token):
+        print("Failing due to invalid token_method or missing auth_token")
+        raise Exception('Unauthorized')
+    # At this point we've confirmed the token format looks ok
+    # So return the unverified token
+    return auth_token
+
+def verify_token(token):
+    # Validate the token to make sure it's authentic
+    jsonurl = urlopen("https://"+AUTH0_DOMAIN+"/.well-known/jwks.json")
+    jwks = json.loads(jsonurl.read())
+    # This currently expects the token to have three distinct sections 
+    # each separated by a period.
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
+    for key in jwks["keys"]:
+        if key["kid"] == unverified_header["kid"]:
+            rsa_key = {
+                "kty": key["kty"],
+                "kid": key["kid"],
+                "use": key["use"],
+                "n": key["n"],
+                "e": key["e"]
+            }
+    if rsa_key:
+        try:  # to validate the jwt
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=["RS256"],
+                audience=AUTH0_API_ID,
+                issuer="https://"+AUTH0_DOMAIN+"/"
+            )
+            print("token validated successfully")
+            return payload
+        except jwt.ExpiredSignatureError:
+            print("Token is expired")
+            raise Exception('Unauthorized')
+        except jwt.JWTClaimsError:
+            print("Token has invalid claims")
+            raise Exception('Unauthorized')
+        except Exception:
+            print("Unable to parse token")
+            raise Exception('Unauthorized')
+        
--- backend/record_song_vote.py
+++ backend/record_song_vote.py
@@ -2,8 +2,6 @@ import boto3
 import os
 import json
 
-from scope_check import scope_check
-
 dynamodb = boto3.client('dynamodb')
 
 def handler(event, context):
@@ -26,3 +24,28 @@ def handler(event, context):
         "body": json.dumps({"votes": result["Attributes"]["votes"]["N"]})
     }
     return response
+
+def scope_check(event, required_scopes=None):
+    print(event)
+    if not required_scopes:
+        raise Exception('No expected scopes specified')
+    if not event['requestContext']['authorizer']['scopes']:
+        raise Exception('No scopes provided')
+    provided_scopes = event['requestContext']['authorizer']['scopes'] 
+    if isinstance(required_scopes, str):
+        # There is only one required_scopes scope to check
+        if required_scopes in provided_scopes:
+            print('Scope check passed for: ' + required_scopes)
+        else: 
+            raise Exception('Scope check failed for: ' + required_scopes)
+    if isinstance(required_scopes, list):
+        # There are one or more scopes to check in an array
+        # If there is more than one provided scope it should 
+        # be separated with a pipe character.
+        provided_scopes = provided_scopes.split('|')
+        if set(required_scopes).issubset(provided_scopes):
+            # If the required scopes are all included in the provided scopes
+            print('Scope check passed for: ' + required_scopes)
+        else:
+            raise Exception('Scope check failed for: ' + required_scopes)
+
